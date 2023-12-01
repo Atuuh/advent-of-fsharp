451ad4860c302b7359e51ad53a183113dbd35da8
Atuuh <not-another-one@hotmail.com>
2023-12-01 18:37:24 +0000

    refactoring

--- src/2023/1.fs
+++ src/2023/1.fs
@@ -5,31 +5,20 @@ open System
 open System.Text.RegularExpressions
 
 let numberRegex = "[0-9]"
-
-let private getFirstNumber value = Regex(numberRegex).Match(value).Value
-
-let private getLastNumber value =
-    Regex(numberRegex, RegexOptions.RightToLeft).Match(value).Value
-
 let stringNumberRegex = "[0-9]|one|two|three|four|five|six|seven|eight|nine"
 
-let private getTextFirstNumber value =
-    Regex(stringNumberRegex).Match(value).Value
+let regexMatch pattern value = Regex(pattern).Match(value).Value
 
-let private getTextLastNumber value =
-    Regex(stringNumberRegex, RegexOptions.RightToLeft).Match(value).Value
+let regexMatchRTL pattern value =
+    Regex(pattern, RegexOptions.RightToLeft).Match(value).Value
+
+let private getFirstNumber = regexMatch numberRegex
+let private getLastNumber = regexMatchRTL numberRegex
+let private getTextFirstNumber = regexMatch stringNumberRegex
+let private getTextLastNumber = regexMatchRTL stringNumberRegex
 
 let stringToNumber value =
     match value with
-    | "1"
-    | "2"
-    | "3"
-    | "4"
-    | "5"
-    | "6"
-    | "7"
-    | "8"
-    | "9" -> value
     | "one" -> "1"
     | "two" -> "2"
     | "three" -> "3"
@@ -39,7 +28,7 @@ let stringToNumber value =
     | "seven" -> "7"
     | "eight" -> "8"
     | "nine" -> "9"
-    | _ -> raise (ArgumentException "Oops")
+    | _ -> value
 
 let private partA =
     Input.toList '\n'
