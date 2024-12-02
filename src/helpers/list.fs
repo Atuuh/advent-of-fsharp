0c365c1d966238e6a7a7b38b697e295afeaeecbf
Atuuh <not-another-one@hotmail.com>
2024-12-02 09:27:28 +0000

    Added helper to get the differences between elements in a list

--- src/helpers/list.fs
+++ src/helpers/list.fs
@@ -16,3 +16,13 @@ let rec transpose xs =
 /// Creates a Map from a list where the keys are unique elements from the list and the values are the amount of times those elements appear within the list.
 let getElementCountMap xs =
     xs |> List.groupBy id |> List.map (fun (a, b) -> a, List.length b) |> Map.ofList
+
+/// Given a list of subtractable items, returns a list of the differences between one item and the next.
+let consecutiveDifference xs =
+    let paired = xs |> List.windowed 2
+
+    paired
+    |> List.map (fun ys ->
+        match ys with
+        | [ x; y ] -> y - x
+        | _ -> failwith "Failure when pairing list for differences")
