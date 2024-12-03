d96bca8a6121ac2669951725d991d536d60814a7
Atuuh <not-another-one@hotmail.com>
2024-12-03 13:48:41 +0000

    Day 3 part 2

--- src/2024/3.fs
+++ src/2024/3.fs
@@ -3,25 +3,71 @@ module AOC.Year2024.Day3
 open System.Text.RegularExpressions
 open AOC
 
-let matchToPairs (m: Match) =
+type Command =
+    | Mul of int * int
+    | Do
+    | Dont
+
+let matchToMult (m: Match) =
     let groups = m.Groups |> List.ofSeq |> List.map (fun group -> group.Value)
-    
-    match groups with 
-    | [_;x;y] -> int x, int y
+
+    match groups with
+    | [ _; x; y ] -> m.Index, Mul(int x, int y)
     | _ -> failwith (sprintf "Couldn't parse input, %A" groups)
 
-let test text = 
-    let matches = Regex.Matches(text, "mul\((\d{1,3}),(\d{1,3})\)")
-    let pairs = List.ofSeq matches |> List.map matchToPairs
-    List.fold (fun acc (x,y)  -> acc + x * y ) 0 pairs
+let parseCommand regex (mapping: Match -> Command) input =
+    Regex.Matches(input, regex)
+    |> List.ofSeq
+    |> List.map (fun m -> m.Index, mapping m)
 
-let private partA input=
-    let answer = test input
+let getCommands input =
+    let mults =
+        Regex.Matches(input, "mul\((\d{1,3}),(\d{1,3})\)")
+        |> List.ofSeq
+        |> List.map matchToMult
+
+    let dos =
+        Regex.Matches(input, "do\(\)") |> List.ofSeq |> List.map (fun m -> m.Index, Do)
+
+    let donts =
+        Regex.Matches(input, "don't\(\)")
+        |> List.ofSeq
+        |> List.map (fun m -> m.Index, Dont)
+
+    List.concat [ mults; dos; donts ] |> List.sortBy (fun (index, _) -> index)
+
+let removeDisabledCommands (commands: list<int * Command>) =
+    commands
+    |> List.fold
+        (fun (coms, enabled) item ->
+            match item, enabled with
+            | (_, Mul _), true -> item :: coms, true
+            | (_, Mul _), false -> coms, false
+            | (_, Do), _ -> coms, true
+            | (_, Dont), _ -> coms, false
+
+        )
+        ([], true)
+    |> fst
+
+let getMulAnswers commands =
+    commands
+    |> List.fold
+        (fun acc (x, y) ->
+            acc
+            + match y with
+              | Mul(a, b) -> a * b
+              | _ -> 0)
+        0
+
+let private partA input =
+    let commands = input |> getCommands
+    let answer = getMulAnswers commands
     printfn "Answer: %i" answer
 
-let private partB =
-    ignore
+let private partB input =
+    let commands = input |> getCommands |> removeDisabledCommands
+    let answer = getMulAnswers commands
+    printfn "Answer: %i" answer
 
 let solution: Types.Solution = { partA = partA; partB = partB }
-
-
