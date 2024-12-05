e40a686bcb092158c8f92f5d3f14566d8c751d97
Atuuh <not-another-one@hotmail.com>
2024-12-05 09:07:52 +0000

    figured out sorting oops

--- src/2024/5.fs
+++ src/2024/5.fs
@@ -17,45 +17,22 @@ let parseInput input =
     |> (fun items ->
         match items with
         | [ rules; pages ] ->
-            let r = rules |> String.splits "\n" |> List.map parseRule
+            let r = rules |> String.splits "\n" |> List.map parseRule |> Set.ofList
             let p = pages |> String.splits "\n" |> List.map parsePages
             r, p
         | _ -> failwith "Error parsing input data")
 
 let getIndex xs x = List.tryFindIndex ((=) x) xs
 
-let isSortedCorrectly rules page =
-    rules
-    |> List.forall (fun (a, b) ->
-        match getIndex page a, getIndex page b with
-        | Some x, Some y -> x < y
-        | _ -> true)
+let compareFn rules a b =
+    if Set.contains (a, b) rules then -1
+    elif Set.contains (b, a) rules then 1
+    else 0
+
+let isSortedCorrectly rules (page: int list) =
+    page |> List.pairwise |> List.forall (fun (a, b) -> compareFn rules a b < 0)
 
 let getMiddle xs = List.item (List.length xs / 2) xs
-
-let print msg x =
-    printfn msg x
-    x
-
-let sortRules rules =
-    let numbersInRules = rules |> List.collect (fun (a, b) -> [ a; b ]) |> List.distinct
-
-    let sorter (a, b) x y =
-        match a, b, x, y with
-        | a, b, x, y when a = x && b = y -> -1
-        | a, b, x, y when a = y && b = x -> -1
-        | _ -> 0
-
-
-    List.fold
-        (fun nums rule -> List.sortWith (sorter rule) nums |> (print "sort step: rule=%A %A" rule))
-        numbersInRules
-        rules
-
-let sortPage rules (a: int) (b: int) =
-    match getIndex rules a, getIndex rules b with
-    | Some x, Some y -> compare x y
-    | _ -> 0
 
 let private partA input =
     let rules, pages = parseInput input
@@ -65,18 +42,13 @@ let private partA input =
 
 let private partB input =
     let rules, pages = parseInput input
-    let incorrectlySortedPages = pages |> List.filter (isSortedCorrectly rules >> not)
-    printfn "Incorrect pages %A" incorrectlySortedPages
-    let sortedRules = sortRules rules
-    printfn "Sorted rules %A" sortedRules
+    let incorrectlySortedPages = pages |> List.filter ((isSortedCorrectly rules) >> not)
 
     let sortedPages =
-        incorrectlySortedPages |> List.map (List.sortWith (sortPage sortedRules))
+        incorrectlySortedPages |> List.map (List.sortWith (compareFn rules))
 
-    printfn "Correct pages %A" sortedPages
     let answer = sortedPages |> List.map getMiddle |> List.sum
     printfn "Answer: %i" answer
 
-let solution: Types.Solution = { partA = partA; partB = partB }
 
-// 97,75,47,61,53,29,13
+let solution: Types.Solution = { partA = partA; partB = partB }
